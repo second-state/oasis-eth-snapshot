f5423e5a1e90e6068aab8908c329729dbd7602a5
hydai <hydai@secondstate.io>
2021-05-11 08:22:11 +0000

    [Epoch 2][Update] Due to the staking format changes, we should handle the special cases by a complex script

--- epoch2-20210404-20210503/compose_information.py
+++ epoch2-20210404-20210503/compose_information.py
@@ -11,6 +11,8 @@ The output format will be:
 """
 
 RUNTIME_ID = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/wM='
+BEFORE_COBALT_UPGRADE = 'before_cobalt_upgrade'
+AFTER_COBALT_UPGRADE = 'after_cobalt_upgrade'
 
 class Entity():
     def __init__(self):
@@ -38,15 +40,26 @@ class Entity():
         self.oasis_id = p[1]
         self.oasis_account = p[2]
         self.eth_address = p[3]
-    def load_stake_amount(self, date, nodes):
-        p = os.path.join(date, 'accounts_info', 'account_info.'+self.oasis_account+'.'+date+'.txt')
-        with open(p) as f:
-            print("Handle ... ", p)
-            lines = f.readlines()
-            print(lines[7].split()[2].strip())
-            self.stake_amount[date] = lines[7].split()[2].strip()
-            if self.oasis_id not in nodes:
-                self.stake_amount[date] = '0'
+    def load_stake_amount(self, prefix, date, nodes):
+        p = os.path.join(prefix, date, 'accounts_info', 'account_info.'+self.oasis_account+'.'+date+'.txt')
+        try:
+            with open(p) as f:
+                print("Handle ... ", p)
+                lines = f.readlines()
+                if prefix == BEFORE_COBALT_UPGRADE:
+                    print(lines[7].split()[2].strip())
+                    self.stake_amount[date] = lines[7].split()[2].strip()
+                else:
+                    targetI = 0;
+                    for i in range(len(lines)):
+                        if lines[i].startswith("Active Delegations to this Account:"):
+                            targetI = i+1
+                            break
+                    print(lines[targetI].split()[1].strip())
+                if self.oasis_id not in nodes:
+                    self.stake_amount[date] = '-1'
+        except:
+            self.stake_amount[date] = '-2'
 
 def load_entities():
     entities = []
@@ -57,17 +70,20 @@ def load_entities():
             entities.append(e)
     return entities
 
-def get_dates():
-    return [p for p in os.listdir('.') if os.path.isdir(os.path.join('.',p))]
+def get_dates(cobalt_upgrade):
+    if cobalt_upgrade == True:
+        return [p for p in os.listdir(AFTER_COBALT_UPGRADE) if os.path.isdir(os.path.join(AFTER_COBALT_UPGRADE,p))]
+    else:
+        return [p for p in os.listdir(BEFORE_COBALT_UPGRADE) if os.path.isdir(os.path.join(BEFORE_COBALT_UPGRADE,p))]
 
 def main():
-    dates = sorted(get_dates())
-    print(dates)
+    before_cobalt_upgrade = sorted(get_dates(False))
+    after_cobalt_upgrade = sorted(get_dates(True))
     entities = load_entities()
     print(entities)
-    for date in dates:
+    for date in before_cobalt_upgrade:
         nodes = {}
-        p = os.path.join(date, 'node_list.'+date+'.json')
+        p = os.path.join(BEFORE_COBALT_UPGRADE, date, 'node_list.'+date+'.json')
         with open(p) as f:
             for _,line in enumerate(f):
                 node = json.loads(line)
@@ -82,11 +98,30 @@ def main():
                 else:
                     continue
         for e in entities:
-            e.load_stake_amount(date, nodes)
+            e.load_stake_amount(BEFORE_COBALT_UPGRADE, date, nodes)
+    for date in after_cobalt_upgrade:
+        nodes = {}
+        p = os.path.join(AFTER_COBALT_UPGRADE, date, 'node_list.'+date+'.json')
+        with open(p) as f:
+            for _,line in enumerate(f):
+                node = json.loads(line)
+                if node['roles'] == 1:
+                    # Compute node
+                    if node['runtimes'][0]['id'] == RUNTIME_ID:
+                        nodes[node['entity_id']] = 1
+                elif node['roles'] == 3:
+                    # Storage + Compute node
+                    if node['runtimes'][0]['id'] == RUNTIME_ID:
+                        nodes[node['entity_id']] = 3
+                else:
+                    continue
+        for e in entities:
+            e.load_stake_amount(AFTER_COBALT_UPGRADE, date, nodes)
     print(entities)
     with open('reward_base.csv', 'w') as f:
         b = ['GitHub_Entity_Filename,Oasis_Entity_ID,Oasis_Account,ETH_Address']
-        b.extend(dates)
+        b.extend(before_cobalt_upgrade)
+        b.extend(after_cobalt_upgrade)
         f.write(','.join(b)+'\n')
         for e in entities:
             f.write(str(e)+'\n')
