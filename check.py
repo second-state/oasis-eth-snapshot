4568642185c673ed3a63d9b02f7d1248f5d83284
dm4 <dm4@secondstate.io>
2021-08-04 17:38:18 +0800

    [Script] Update check script

--- check.py
+++ check.py
@@ -3,17 +3,17 @@
 import json
 import os
 import subprocess
+import sys
 
-RUNTIME_ID = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/wM='
-TARGET_DATETIME = '2021-03-08-05-00-02'
+RUNTIME_ID = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/wQ='
 
 def find_json_by_entity_id(entity_id):
     command = f'grep -rl {entity_id} ../oasis-ssvm-paratime-entities/mainnet'
     json_file = subprocess.check_output(command, shell=True).decode('utf-8').strip()
     return json_file
 
-def check_validator_by_entity_id(entity_id):
-    command = f'grep -rl "{entity_id}" {TARGET_DATETIME}/accounts_info'
+def check_validator_by_entity_id(target_datetime, entity_id):
+    command = f'grep -rl "{entity_id}" {target_datetime}/accounts_info'
     account_info_file = subprocess.check_output(command, shell=True).decode('utf-8').strip()
     command = f'grep "Global: node-validator" {account_info_file}'
     returncode = subprocess.run(command, shell=True, capture_output=True).returncode
@@ -27,9 +27,14 @@ def get_paratime_entities():
     return output.split('\n')
 
 def main():
+    if len(sys.argv) != 2:
+        print(f'Usage: {sys.argv[0]} <target_datetime>')
+        sys.exit(1)
+
+    target_datetime = sys.argv[1]
     whitelist = get_paratime_entities()
 
-    with open(f'{TARGET_DATETIME}/node_list.{TARGET_DATETIME}.json') as f:
+    with open(f'{target_datetime}/node_list.{target_datetime}.json') as f:
         nodelist = f.readlines()
         nodelist = list(map(json.loads, nodelist))
 
@@ -55,7 +60,7 @@ def main():
     print(f'Run SSVM ({len(entity_running_ssvm)})')
     for entity in entity_running_ssvm:
         filename = find_json_by_entity_id(entity).split('/')[-1]
-        print(f'{entity} / {check_validator_by_entity_id(entity)} / {filename}')
+        print(f'{entity} / {check_validator_by_entity_id(target_datetime, entity)} / {filename}')
     print()
     print(f'No Run SSVM ({len(entity_without_running_ssvm)})')
     for entity in entity_without_running_ssvm:
